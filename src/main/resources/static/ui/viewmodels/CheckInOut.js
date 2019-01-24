f00047c883f1783dac5e5b87587928d4fc990a41
chenjiefei <nlpx_dc@hotmail.com>
2019-01-24 09:12:02 +0800

    add checkinout page

---
+++ src/main/resources/static/ui/viewmodels/CheckInOut.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    },
+    methods:{
+        handleCheckInOut(){
+            console.log('check inout click');            
+        }
+    }
+});
