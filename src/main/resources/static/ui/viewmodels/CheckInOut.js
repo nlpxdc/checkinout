567a5762511c739b50d3dd28fa876d3403b5ae6e
chenjiefei <nlpx_dc@hotmail.com>
2019-01-24 10:36:31 +0800

    draft load weixin js

--- src/main/resources/static/ui/viewmodels/CheckInOut.js
+++ src/main/resources/static/ui/viewmodels/CheckInOut.js
@@ -1,3 +1,25 @@
+const ticket = 'LIKLckvwlJT9cWIhEQTwfJuU557garB7J1tppEvMocW-iUiFaI9vUMg2qx3lzwqT3mpp5OI-QyR6_PBudG1D9Q';
+
+wx.config({
+    debug: false,
+    appId: 'wx0c14a6dfeab19166', // 必填，公众号的唯一标识
+    timestamp: 1548295875, // 必填，生成签名的时间戳
+    nonceStr: '1234567890abcdef', // 必填，生成签名的随机串
+    signature: '62e373fd5534a4d3edafa28462bf64b21c7a43f6',// 必填，签名
+    jsApiList: [
+        'checkJsApi'
+    ] // 必填，需要使用的JS接口列表
+});
+
+wx.error(function(error){
+    console.error(error);
+});
+
+wx.ready(function(){
+    console.log('验证成功');
+});
+
+
 var app = new Vue({
     el: '#app',
     data: {
