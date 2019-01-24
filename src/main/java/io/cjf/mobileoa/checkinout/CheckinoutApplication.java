9940fe69a3384378ff1f1f74f906be0fa51f62fc
chenjiefei <nlpx_dc@hotmail.com>
2019-01-24 08:48:09 +0800

    first init

---
+++ src/main/java/io/cjf/mobileoa/checkinout/CheckinoutApplication.java
@@ -0,0 +1,14 @@
+package io.cjf.mobileoa.checkinout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckinoutApplication.class, args);
+    }
+
+}
+
