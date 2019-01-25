7ec0da74334762711e1e1c320545f98f41ed4f58
chenjiefei <nlpx_dc@hotmail.com>
2019-01-25 14:20:48 +0800

    test message receive api 4

--- src/main/java/io/cjf/mobileoa/checkinout/controller/MessageController.java
+++ src/main/java/io/cjf/mobileoa/checkinout/controller/MessageController.java
@@ -18,7 +18,7 @@ public class MessageController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/receive", produces = MediaType.APPLICATION_XML_VALUE)
-    public MessageTextDTO receive(@RequestBody(required = false) String jsonObject, @RequestParam Map<String,String> allParams, @RequestParam(required = false) String echostr){
+    public String receive(@RequestBody(required = false) String jsonObject, @RequestParam Map<String,String> allParams, @RequestParam(required = false) String echostr){
         logger.info("{}", jsonObject);
         logger.info("{}", allParams);
         MessageTextDTO messageTextDTO = new MessageTextDTO();
@@ -27,7 +27,17 @@ public class MessageController {
         messageTextDTO.setCreateTime(new Date().getTime());
         messageTextDTO.setMsgType("text");
         messageTextDTO.setContent("lalala");
-        return messageTextDTO;
+
+        String retStr = "<xml>\n" +
+                "    <Content>lalala</Content>\n" +
+                "    <ToUserName>oUwXe58JsPM6MBFsI3YvnbFIpg-8</ToUserName>\n" +
+                "    <FromUserName>gh_aad09ca98ab8</FromUserName>\n" +
+                "    <MsgType>text</MsgType>\n" +
+                "    <CreateTime>1548396765780</CreateTime>\n" +
+                "</xml>";
+
+
+        return retStr;
         //return echostr;
     }
 }
