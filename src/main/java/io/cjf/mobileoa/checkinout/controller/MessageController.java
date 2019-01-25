29fcc795aa12a06dea09613cc46c680f44d9b6d5
chenjiefei <nlpx_dc@hotmail.com>
2019-01-25 14:01:02 +0800

    test message receive api 1

--- src/main/java/io/cjf/mobileoa/checkinout/controller/MessageController.java
+++ src/main/java/io/cjf/mobileoa/checkinout/controller/MessageController.java
@@ -2,10 +2,12 @@ package io.cjf.mobileoa.checkinout.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import io.cjf.mobileoa.checkinout.dto.MessageTextDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.Map;
 
 @RestController
@@ -15,9 +17,16 @@ public class MessageController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/receive")
-    public String receive(@RequestBody(required = false) String jsonObject, @RequestParam Map<String,String> allParams, @RequestParam(required = false) String echostr){
+    public MessageTextDTO receive(@RequestBody(required = false) String jsonObject, @RequestParam Map<String,String> allParams, @RequestParam(required = false) String echostr){
         logger.info("{}", jsonObject);
         logger.info("{}", allParams);
-        return echostr;
+        MessageTextDTO messageTextDTO = new MessageTextDTO();
+        messageTextDTO.setToUserName("oUwXe58JsPM6MBFsI3YvnbFIpg-8");
+        messageTextDTO.setFromUserName("gh_aad09ca98ab8");
+        messageTextDTO.setCreateTime(new Date().getTime());
+        messageTextDTO.setMsgType("text");
+        messageTextDTO.setContent("lalala");
+        return messageTextDTO;
+        //return echostr;
     }
 }
