5f286dbe4b97451076d77b64ad707b2db4ce7b0a
chenjiefei <nlpx_dc@hotmail.com>
2019-01-25 14:03:05 +0800

    test message receive api 2

---
+++ src/main/java/io/cjf/mobileoa/checkinout/dto/MessageTextDTO.java
@@ -0,0 +1,49 @@
+package io.cjf.mobileoa.checkinout.dto;
+
+public class MessageTextDTO {
+    private String ToUserName;
+    private String FromUserName;
+    private Long CreateTime;
+    private String MsgType;
+    private String Content;
+
+    public String getToUserName() {
+        return ToUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        ToUserName = toUserName;
+    }
+
+    public String getFromUserName() {
+        return FromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        FromUserName = fromUserName;
+    }
+
+    public Long getCreateTime() {
+        return CreateTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        CreateTime = createTime;
+    }
+
+    public String getMsgType() {
+        return MsgType;
+    }
+
+    public void setMsgType(String msgType) {
+        MsgType = msgType;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+}
